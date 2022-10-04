4a93fca60c658a9a3858cd8af0b86b3a4b6de3d8
Adrian Valdivia <vadrianenrique@gmail.com>
2022-10-04 15:18:10 -0500

    first commit

---
+++ script-inserts.sql
@@ -0,0 +1,14 @@
+INSERT INTO gerentes(nombre,    
+ap_paterno,
+ap_materno,
+telefono  ) VALUES ("Adrian Enrique", "Valdivia", "Cabañas", "5511980471");
+
+INSERT INTO gerentes(nombre,    
+ap_paterno,
+ap_materno,
+telefono  ) VALUES ("Armando", "Hernandez", "Martinez", "5598154610");
+
+INSERT INTO gerentes(nombre,    
+ap_paterno,
+ap_materno,
+telefono  ) VALUES ("Heyrisw Joseline", "Nophal", "Silva", "5598158899");
